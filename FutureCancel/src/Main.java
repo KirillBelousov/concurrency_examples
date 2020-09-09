950d66ec76ac8d0c1be591b237eab4630ef977c3
Alexander Kulyakhtin <akulyakhtin@gmail.com>
2020-09-09 10:47:54 +0300

    concurrency samples

---
+++ FutureCancel/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Main {
+
+    static class MyRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println("Starting");
+            for(long i=0; i<100000L; ++i) {
+                if(Thread.interrupted()) {
+                    System.out.println("Interrupted! last number: " + i);
+                    break;
+                }
+                System.out.println(i);
+            }
+            System.out.println("Done");
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ExecutorService es = Executors.newSingleThreadExecutor();
+        Future<?> f = es.submit(new MyRunnable());
+        Thread.sleep(20);
+        f.cancel(true);
+    }
+}
